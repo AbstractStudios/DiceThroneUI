f7d4398cdd98f2c17320257ba6853cda639ac0b2
Josh Elliott <josh@joshelliott.me>
2023-09-12 08:00:43 -0400

    Updating to use the actual roll endpoint (untested)

--- src/stores/playerStore.ts
+++ src/stores/playerStore.ts
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import axios from 'axios';
 
+const BASE_URL = 'https://back-end-azdjtklhyq-uc.a.run.app';
+
 export const usePlayerStore = defineStore({
     id: 'player',
     state: () => ({
@@ -18,15 +20,19 @@ export const usePlayerStore = defineStore({
     },
     actions: {
         async fetchPlayers(gameId: string) {
-            const response = await axios.get(`https://back-end-azdjtklhyq-uc.a.run.app/v1/games/${gameId}`)
+            const response = await axios.get(`${BASE_URL}/v1/games/${gameId}`)
             if (response.status !== 200) {
                 throw new Error('Failed to fetch players');
             }
 
             this.players = response.data.players;
         },
-        async rollDice(gameId: string, playerId: string) {
-
+        async rollDice(playerId: string) {
+            const response = await axios.post(`${BASE_URL}/v1/players/${playerId}/dice/roll`)
+            console.log(response);
+            if (response.status !== 200) {
+                throw new Error('Failed to roll dice');
+            }
         }
     }
 });
